20bafaeeb5234f4c6d1f7fcc69a7db11cd48e51c
Jin Hui <jinhui96@hotmail.com>
2017-11-03 23:29:54 +0800

    Updated Detail Event Picture Zoom

--- app/src/main/java/com/example/user/myproject/DetailEventActivity.java
+++ app/src/main/java/com/example/user/myproject/DetailEventActivity.java
@@ -1,6 +1,10 @@
 package com.example.user.myproject;
 
 import android.Manifest;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Dialog;
 import android.app.DownloadManager;
 import android.app.ProgressDialog;
@@ -11,6 +15,8 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -23,12 +29,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ZoomControls;
 
 import com.example.user.myproject.Modal.Action;
 import com.example.user.myproject.Modal.ApplicationEvent;
@@ -78,6 +88,8 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
     String from;
     EventRegistration reg;
     ProgressDialog pd;
+    private Animator mCurrentAnimator;
+    private int mShortAnimationDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,7 +102,144 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         studentId = new SessionManager(this).getUserDetails().get("id");
+    }
 
+    private void zoomImageFromThumb(final View thumbView, Bitmap imageResId) {
+        // If there's an animation in progress, cancel it
+        // immediately and proceed with this one.
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        // Load the high-resolution "zoomed-in" image.
+        final ImageView expandedImageView = (ImageView) findViewById(
+                R.id.expanded_image);
+        expandedImageView.setImageBitmap(imageResId);
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        // This step involves lots of math. Yay, math.
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the container
+        // view. Also set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.container)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        // Adjust the start bounds to be the same aspect ratio as the final
+        // bounds using the "center crop" technique. This prevents undesirable
+        // stretching during the animation. Also calculate the start scaling
+        // factor (the end scaling factor is always 1.0).
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it will position the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations
+        // to the top-left corner of the zoomed-in view (the default
+        // is the center of the view).
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties (X, Y, SCALE_X, and SCALE_Y).
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
+                View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        // Upon clicking the zoomed-in image, it should zoom back down
+        // to the original bounds and show the thumbnail instead of
+        // the expanded image.
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y,startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        thumbView.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
     }
 
     @Override
@@ -544,7 +693,7 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
                 txtGroupInfo.setText("Group Available: " + event.getCurrentGroup() + " / "+ event.getMaxGroup());
                 pd.dismiss();
 
-                ImageView image = (ImageView)findViewById(R.id.imageDetailEvent);
+                ImageButton image = (ImageButton)findViewById(R.id.imageDetailEvent);
 
                 ImageTask task = new ImageTask(image);
                 int tempTimetable = 0;
@@ -554,6 +703,17 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
                     ex.printStackTrace();
                     tempTimetable = 0;
                 }
+
+                final View thumb1View = findViewById(R.id.imageDetailEvent);
+                thumb1View.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        zoomImageFromThumb(thumb1View, (Bitmap)((ImageButton)findViewById(R.id.imageDetailEvent)).getTag());
+                    }
+                });
+
+                // Retrieve and cache the system's default "short" animation time.
+                mShortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
 
                 task.execute(tempTimetable);
@@ -655,11 +815,11 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
 
     private class ImageTask extends AsyncTask<Integer, Void, Bitmap>
     {
-        private final WeakReference<ImageView> imageViewReference;
+        private final WeakReference<ImageButton> imageViewReference;
 
-        public ImageTask(ImageView imageView) {
+        public ImageTask(ImageButton imageView) {
 
-            imageViewReference = new WeakReference<ImageView>(imageView);
+            imageViewReference = new WeakReference<ImageButton>(imageView);
         }
 
         protected void onPreExecute() {
@@ -684,12 +844,14 @@ public class DetailEventActivity extends AppCompatActivity implements OnMapReady
 
         protected void onPostExecute(Bitmap result) {
             if (imageViewReference != null && result != null) {
-                final ImageView imageView = imageViewReference.get();
+                final ImageButton imageView = imageViewReference.get();
                 if (imageView != null) {
                     imageView.setImageBitmap(result);
+                    imageView.setTag(result);
                 } else {
                     // if you see  dao then change to icnoimage icon
-                    imageView.setImageResource(R.mipmap.ic_launcher);
+                    imageView.setImageResource(R.mipmap.ic_noimage);
+                    //imageView.setTag(result);
                 }
             }
         }
